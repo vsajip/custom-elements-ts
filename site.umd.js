832db3df11275bb30e0e4c28a6d353f28e7fe008
vsajip <vsajip@users.noreply.github.com>
2026-03-20 09:31:39 +0000

    deploy: 1e5d10853ec9f8053fcdd35db64c645616caee27

--- site.umd.js
+++ site.umd.js
@@ -1,23 +1,25 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
     typeof define === 'function' && define.amd ? define(['exports'], factory) :
-    (factory((global.Site = {})));
-}(this, (function (exports) { 'use strict';
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.Site = {}));
+})(this, (function (exports) { 'use strict';
 
-    /*! *****************************************************************************
-    Copyright (c) Microsoft Corporation. All rights reserved.
-    Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-    this file except in compliance with the License. You may obtain a copy of the
-    License at http://www.apache.org/licenses/LICENSE-2.0
+    /******************************************************************************
+    Copyright (c) Microsoft Corporation.
 
-    THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-    KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
-    WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
-    MERCHANTABLITY OR NON-INFRINGEMENT.
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted.
 
-    See the Apache Version 2.0 License for specific language governing permissions
-    and limitations under the License.
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+    REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+    LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+    OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+    PERFORMANCE OF THIS SOFTWARE.
     ***************************************************************************** */
+    /* global Reflect, Promise, SuppressedError, Symbol, Iterator */
+
 
     function __decorate(decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -30,19 +32,34 @@
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(metadataKey, metadataValue);
     }
 
-    const toKebabCase = str => {
+    typeof SuppressedError === "function" ? SuppressedError : function (error, suppressed, message) {
+        var e = new Error(message);
+        return e.name = "SuppressedError", e.error = error, e.suppressed = suppressed, e;
+    };
+
+    const toKebabCase = (str) => {
         return str
             .replace(/([a-z])([A-Z])/g, '$1-$2')
             .replace(/[\s_]+/g, '-')
             .toLowerCase();
     };
-    const toCamelCase = str => {
-        return str
-            .toLowerCase()
-            .replace(/(\-\w)/g, (m) => m[1].toUpperCase());
+    const toCamelCase = (str) => {
+        return str.toLowerCase().replace(/(-\w)/g, (m) => m[1].toUpperCase());
     };
     const tryParseInt = (value) => {
-        return (parseInt(value) == value && parseFloat(value) !== NaN) ? parseInt(value) : value;
+        if (typeof value === 'number' && Number.isInteger(value)) {
+            return value;
+        }
+        if (typeof value === 'string') {
+            const trimmed = value.trim();
+            if (trimmed !== '') {
+                const parsed = Number(trimmed);
+                if (Number.isInteger(parsed) && String(parsed) === trimmed) {
+                    return parsed;
+                }
+            }
+        }
+        return value;
     };
 
     const Listen = (eventName, selector) => {
@@ -50,17 +67,21 @@
             if (!target.constructor.listeners) {
                 target.constructor.listeners = [];
             }
-            target.constructor.listeners.push({ selector: selector, eventName: eventName, handler: target[methodName] });
+            target.constructor.listeners.push({
+                selector: selector,
+                eventName: eventName,
+                handler: target[methodName],
+            });
         };
     };
     const addEventListeners = (target) => {
         if (target.constructor.listeners) {
             const targetRoot = target.shadowRoot || target;
             for (const listener of target.constructor.listeners) {
-                const eventTarget = (listener.selector)
-                    ? targetRoot.querySelector(listener.selector)
-                        ? targetRoot.querySelector(listener.selector) : null
-                    : target;
+                let eventTarget = target;
+                if (listener.selector) {
+                    eventTarget = targetRoot.querySelector(listener.selector);
+                }
                 if (eventTarget) {
                     eventTarget.addEventListener(listener.eventName, (e) => {
                         listener.handler.call(target, e);
@@ -74,7 +95,8 @@
         return (target, propName) => {
             const attrName = toKebabCase(propName);
             function get() {
-                if (this.props[propName]) {
+                const hasOwn = Object.prototype.hasOwnProperty.call(this.props, propName);
+                if (hasOwn) {
                     return this.props[propName];
                 }
                 return this.getAttribute(attrName);
@@ -83,7 +105,9 @@
                 if (this.__connected) {
                     const oldValue = this.props[propName];
                     this.props[propName] = tryParseInt(value);
-                    if (typeof value != 'object') {
+                    const valueType = typeof value;
+                    const shouldReflect = valueType === 'string' || valueType === 'number' || valueType === 'boolean';
+                    if (shouldReflect) {
                         this.setAttribute(attrName, value);
                     }
                     else {
@@ -106,20 +130,22 @@
     const getProps = (target) => {
         const watchAttributes = target.constructor.watchAttributes;
         const plainAttributes = Object.assign({}, watchAttributes);
-        Object.keys(plainAttributes).forEach(v => plainAttributes[v] = '');
-        const cycleProps = Object.assign({}, plainAttributes, target.constructor.propsInit);
+        Object.keys(plainAttributes).forEach((v) => (plainAttributes[v] = ''));
+        const cycleProps = Object.assign(Object.assign({}, plainAttributes), target.constructor.propsInit);
         return Object.keys(cycleProps);
     };
     const initializeProps = (target) => {
         const watchAttributes = target.constructor.watchAttributes;
-        for (let prop of getProps(target)) {
+        for (const prop of getProps(target)) {
             if (watchAttributes) {
-                if (watchAttributes[toKebabCase(prop)] == null) {
+                if (watchAttributes[toKebabCase(prop)] === null ||
+                    watchAttributes[toKebabCase(prop)] === undefined) {
                     watchAttributes[toKebabCase(prop)] = '';
                 }
                 else {
-                    const attribValue = target.props[prop] || target.getAttribute(toKebabCase(prop));
-                    if (typeof target[watchAttributes[prop]] == 'function') {
+                    const hasOwn = Object.prototype.hasOwnProperty.call(target.props, prop);
+                    const attribValue = hasOwn ? target.props[prop] : target.getAttribute(toKebabCase(prop));
+                    if (typeof target[watchAttributes[prop]] === 'function') {
                         target[watchAttributes[prop]]({ new: attribValue });
                     }
                 }
@@ -134,57 +160,60 @@
 
     const CustomElement = (args) => {
         return (target) => {
-            var _a;
             const tag = args.tag || toKebabCase(target.prototype.constructor.name);
-            const customElement = (_a = class extends target {
-                    constructor() {
-                        super();
-                        this.props = {};
-                        this.showShadowRoot = args.shadow == null ? true : args.shadow;
-                        if (!this.shadowRoot && this.showShadowRoot) {
-                            this.attachShadow({ mode: 'open' });
+            const customElement = class extends target {
+                static get observedAttributes() {
+                    return Object.keys(this.propsInit || {}).map((x) => toKebabCase(x));
+                }
+                constructor() {
+                    super();
+                    this.__connected = false;
+                    this.props = {};
+                    this.showShadowRoot =
+                        args.shadow === undefined || args.shadow === null ? true : args.shadow;
+                    if (!this.shadowRoot && this.showShadowRoot) {
+                        this.attachShadow({ mode: 'open' });
+                    }
+                }
+                attributeChangedCallback(name, oldValue, newValue) {
+                    this.onAttributeChange(name, oldValue, newValue);
+                }
+                onAttributeChange(name, oldValue, newValue, set = true) {
+                    if (oldValue !== newValue) {
+                        if (set) {
+                            const propName = toCamelCase(name);
+                            this[propName] = newValue;
                         }
-                    }
-                    static get observedAttributes() {
-                        return Object.keys(this.propsInit || {}).map(x => toKebabCase(x));
-                    }
-                    attributeChangedCallback(name, oldValue, newValue) {
-                        this.onAttributeChange(name, oldValue, newValue);
-                    }
-                    onAttributeChange(name, oldValue, newValue, set = true) {
-                        if (oldValue != newValue) {
-                            if (set) {
-                                this[toCamelCase(name)] = newValue;
-                            }
-                            const watchAttributes = this.constructor.watchAttributes;
-                            if (watchAttributes && watchAttributes[name]) {
-                                const methodToCall = watchAttributes[name];
-                                if (this.__connected) {
-                                    if (typeof this[methodToCall] == 'function') {
-                                        this[methodToCall]({ old: oldValue, new: newValue });
-                                    }
+                        const watchAttributes = this.constructor.watchAttributes;
+                        if (watchAttributes && watchAttributes[name]) {
+                            const methodToCall = watchAttributes[name];
+                            if (this.__connected) {
+                                if (typeof this[methodToCall] === 'function') {
+                                    this[methodToCall]({ old: oldValue, new: newValue });
                                 }
                             }
                         }
                     }
-                    connectedCallback() {
-                        this.__render();
-                        super.connectedCallback && super.connectedCallback();
-                        this.__connected = true;
-                        addEventListeners(this);
-                        initializeProps(this);
+                }
+                connectedCallback() {
+                    this.__render();
+                    const parentProto = Object.getPrototypeOf(Object.getPrototypeOf(this)) || null;
+                    if (parentProto && typeof parentProto.connectedCallback === 'function') {
+                        parentProto.connectedCallback.call(this);
                     }
-                    __render() {
-                        if (this.__connected)
-                            return;
-                        const template = document.createElement('template');
-                        const style = `${args.style ? `<style>${args.style}</style>` : ''}`;
-                        template.innerHTML = `${style}${args.template ? args.template : ''}`;
-                        (this.showShadowRoot ? this.shadowRoot : this).appendChild(document.importNode(template.content, true));
-                    }
-                },
-                _a.__connected = false,
-                _a);
+                    this.__connected = true;
+                    addEventListeners(this);
+                    initializeProps(this);
+                }
+                __render() {
+                    if (this.__connected)
+                        return;
+                    const template = document.createElement('template');
+                    const style = `${args.style ? `<style>${args.style}</style>` : ''}`;
+                    template.innerHTML = `${style}${args.template ? args.template : ''}`;
+                    (this.showShadowRoot ? this.shadowRoot : this).appendChild(document.importNode(template.content, true));
+                }
+            };
             if (!customElements.get(tag)) {
                 customElements.define(tag, customElement);
             }
@@ -231,7 +260,169 @@ export class MessageElement extends HTMLElement {
         CustomElement({
             tag: 'cts-code-example',
             template: '<div id="code"></div>',
-            style: ':host{font-size:15px;padding:24px;display:block;overflow-x:auto;color:#bbb;max-height:500px}@media screen and (min-width:1000px){:host{font-size:17px;padding:32px}}code[class*="language-"]{color:#c5c8c6;text-shadow:0 1px rgba(0,0,0,0.3);font-family:Inconsolata,Monaco,Consolas,"Courier New",Courier,monospace;direction:ltr;text-align:left;white-space:pre;word-spacing:normal;word-break:normal;line-height:1.5;-moz-tab-size:4;-o-tab-size:4;tab-size:4;-webkit-hyphens:none;-moz-hyphens:none;-ms-hyphens:none;hyphens:none}pre{margin:0}pre[class*="language-"]{color:#c5c8c6;text-shadow:0 1px rgba(0,0,0,0.3);font-family:Inconsolata,Monaco,Consolas,"Courier New",Courier,monospace;direction:ltr;text-align:left;white-space:pre;word-spacing:normal;word-break:normal;line-height:1.5;-moz-tab-size:4;-o-tab-size:4;tab-size:4;-webkit-hyphens:none;-moz-hyphens:none;-ms-hyphens:none;hyphens:none;padding:1em;overflow:auto;border-radius:.3em}:not(pre)>code[class*="language-"],pre[class*="language-"]{background:#1d1f21}:not(pre)>code[class*="language-"]{padding:.1em;border-radius:.3em}.token.comment,.token.prolog,.token.doctype,.token.cdata{color:#7c7c7c}.token.punctuation{color:#c5c8c6}.namespace{opacity:.7}.token.property,.token.keyword,.token.tag{color:#e06c75}.token.class-name{color:#ffffb6;text-decoration:underline}.token.boolean,.token.constant{color:#9c9}.token.symbol,.token.deleted{color:#f92672}.token.number{color:#ff73fd}.token.selector,.token.attr-name,.token.string,.token.char,.token.builtin,.token.inserted{color:#a8ff60}.token.variable{color:#c6c5fe}.token.operator{color:#ededed}.token.entity{color:#ffffb6}.token.url{color:#96cbfe}.language-css .token.string,.style .token.string{color:#87c38a}.token.atrule,.token.attr-value{color:#f9ee98}.token.function{color:#dad085}.token.regex{color:#e9c062}.token.important{color:#fd971f;font-weight:bold}.token.bold{font-weight:bold}.token.italic{font-style:italic}.token.entity{cursor:help}'
+            style: `
+:host {
+  font-size: 15px;
+  padding: 24px;
+  display: block;
+  @media screen and (min-width: 1000px) {
+    font-size: 17px;
+    padding: 32px;
+  }
+  overflow-x: auto;
+  color: rgb(187,187,187);
+  max-height: 500px;
+}
+
+code[class*="language-"] {
+  color: #c5c8c6;
+  text-shadow: 0 1px rgba(0, 0, 0, 0.3);
+  font-family: Inconsolata, Monaco, Consolas, "Courier New", Courier, monospace;
+  direction: ltr;
+  text-align: left;
+  white-space: pre;
+  word-spacing: normal;
+  word-break: normal;
+  line-height: 1.5;
+  -moz-tab-size: 4;
+  -o-tab-size: 4;
+  tab-size: 4;
+  -webkit-hyphens: none;
+  -moz-hyphens: none;
+  -ms-hyphens: none;
+  hyphens: none;
+}
+
+pre {
+  margin: 0;
+}
+
+pre[class*="language-"] {
+  color: #c5c8c6;
+  text-shadow: 0 1px rgba(0, 0, 0, 0.3);
+  font-family: Inconsolata, Monaco, Consolas, "Courier New", Courier, monospace;
+  direction: ltr;
+  text-align: left;
+  white-space: pre;
+  word-spacing: normal;
+  word-break: normal;
+  line-height: 1.5;
+  -moz-tab-size: 4;
+  -o-tab-size: 4;
+  tab-size: 4;
+  -webkit-hyphens: none;
+  -moz-hyphens: none;
+  -ms-hyphens: none;
+  hyphens: none;
+  padding: 1em;
+  overflow: auto;
+  border-radius: 0.3em;
+}
+
+/* Code blocks */
+
+:not(pre)>code[class*="language-"],
+pre[class*="language-"] {
+  background: #1d1f21;
+}
+
+/* Inline code */
+
+:not(pre)>code[class*="language-"] {
+  padding: .1em;
+  border-radius: .3em;
+}
+
+.token {
+  &.comment,
+  &.prolog,
+  &.doctype,
+  &.cdata {
+    color: #7C7C7C;
+  }
+  &.punctuation {
+    color: #c5c8c6;
+  }
+}
+
+.namespace {
+  opacity: .7;
+}
+
+.token {
+  &.property,
+  &.keyword,
+  &.tag {
+    color: rgb(224, 108, 117);
+  }
+  &.class-name {
+    color: #FFFFB6;
+    text-decoration: underline;
+  }
+  &.boolean,
+  &.constant {
+    color: #99CC99;
+  }
+  &.symbol,
+  &.deleted {
+    color: #f92672;
+  }
+  &.number {
+    color: #FF73FD;
+  }
+  &.selector,
+  &.attr-name,
+  &.string,
+  &.char,
+  &.builtin,
+  &.inserted {
+    color: #A8FF60;
+  }
+  &.variable {
+    color: #C6C5FE;
+  }
+  &.operator {
+    color: #EDEDED;
+  }
+  &.entity {
+    color: #FFFFB6;
+    /* text-decoration: underline; */
+  }
+  &.url {
+    color: #96CBFE;
+  }
+}
+
+.language-css .token.string,
+.style .token.string {
+  color: #87C38A;
+}
+
+.token {
+  &.atrule,
+  &.attr-value {
+    color: #F9EE98;
+  }
+  &.function {
+    color: #DAD085;
+  }
+  &.regex {
+    color: #E9C062;
+  }
+  &.important {
+    color: #fd971f;
+    font-weight: bold;
+  }
+  &.bold {
+    font-weight: bold;
+  }
+  &.italic {
+    font-style: italic;
+  }
+  &.entity {
+    cursor: help;
+  }
+}`
         }),
         __metadata("design:paramtypes", [])
     ], exports.CodeExampleElement);
@@ -279,8 +470,5 @@ export class MessageElement extends HTMLElement {
         })
     ], exports.MessageElement);
 
-    Object.defineProperty(exports, '__esModule', { value: true });
-
-})));
-
+}));
 //# sourceMappingURL=site.umd.js.map
